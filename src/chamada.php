2075fb2df22bc0eca8d540ae314cd5fcd1011434
Nicolas Antonio Smaniotto <49732409+Nicolassmaniotto@users.noreply.github.com>
2021-04-29 12:46:18 -0300

    Update chamada.php

--- src/chamada.php
+++ src/chamada.php
@@ -4,8 +4,10 @@ function chamada(){
     include_once './function_php/pega_response_json.php';
     include_once './function_php/json_request.php';
     include_once './function_php/request_to_google_home.php';
-
+    
     try{
+        if(!json_request_google();){
+        };
         // $local_json = './json/response_example.json';
         $local_json = './json/request_example.json';
         // pega_response_json($local_json,false);
